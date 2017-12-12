c22a9b8679af87d7118c19dc5705f80bff02df8e
Sourav Singh <ssouravsingh12@gmail.com>
2017-12-12 17:38:53 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,4 +1,22 @@
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, Input, ZeroPadding2D, \
-    Dropout, Conv2DTranspose, Cropping2D, Add
-    
+from keras.layers import Conv2D, MaxPooling2D, Input, Dropout, Conv2DTranspose, LSTM, Add
+import numpy as np
+
+
+
+
+
+model.add(Dropout(0.1))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(Dropout(0.1))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(Dropout(0.1))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(Dropout(0.1))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(Dropout(0.1))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
+model.add(Dropout(0.1))
+model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_values)))
